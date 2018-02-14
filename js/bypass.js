ac52fc836cf7aaf88d3e88d8df34ae81ff3c6eb2
Cameron Eldridge <cameron@mad.man>
2018-02-14 16:11:14 +1100

    Refactor for new paywall

--- js/bypass.js
+++ js/bypass.js
@@ -1,9 +1,33 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {
-            cancel: details.url.indexOf("hagrid") != -1
-        };
-    },
-    {urls: ["<all_urls>"]},
-    ["blocking"]
-);
+console.log('Running background script');
+
+let observer;
+
+function mutationObserve() {
+    console.log('Running mutation observer');
+    observer.observe(document.body, { childList: false, subtree:false, attributes: true, attributeFilter: ['class'] });
+}
+
+function mutationDisconnect() {
+    observer.disconnect();
+}
+
+function handleMutation(mutations) {
+    mutationDisconnect();
+    mutations.forEach(mutation => {
+        // console.log(mutation);
+        // console.log($(mutation.target).hasClass('lock'));
+        if($(mutation.target).hasClass('lock')){
+            $('article.paywall').siblings().each(function(){
+                $(this).remove();
+                console.log('Removed paywall element ' + this);
+            });
+            $('body').removeClass('lock');
+        }
+    });
+    mutationObserve();
+}
+
+observer = new MutationObserver(handleMutation);
+mutationObserve();
+
+
